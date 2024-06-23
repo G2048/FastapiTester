49163d6efbb459bc85f3d674b8a61591e459516a
IX <borntokill.ix@gmail.com>
2024-06-23 21:10:53 +0700

    feature(api): add base classe for interaction with any http api

---
+++ api/_base.py
@@ -0,0 +1,48 @@
+from json import JSONDecodeError
+
+from configs import get_logger
+from httpx import AsyncClient
+
+logger = get_logger()
+
+
+class BaseApi:
+    def __init__(self, url):
+        self.HEADERS = {'Content-Type': 'application/json'}
+        self.URL = url
+
+    @staticmethod
+    def _validateJson(jsondata):
+        try:
+            return jsondata()
+        except JSONDecodeError:
+            return None
+
+    async def _request(self, url, method='GET', query_params=None, **kwargs):
+        self.URL += url
+        logger.debug(f'{url}, {query_params=}, {self.HEADERS=}')
+        async with AsyncClient(follow_redirects=True) as client:
+            response = await client.request(
+                method=method, url=self.URL, params=query_params, headers=self.HEADERS, **kwargs
+            )
+            logger.debug(response.status_code)
+            logger.debug(response.text)
+
+            if response.status_code < 300:
+                response_json = self._validateJson(response.json)
+                if response_json:
+                    return response_json
+
+
+async def test():
+    site = 'news.ycombinator.com'
+    url = f'https://{site}/'
+    api = BaseApi(url)
+    answer = await api._request('')
+    print(answer)
+
+
+if __name__ == '__main__':
+    import asyncio
+
+    asyncio.run(test())
